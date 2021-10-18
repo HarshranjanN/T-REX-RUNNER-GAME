ab88e3372c570ab5982bfc65c56b2f0370911f1d
HarshranjanN <92657798+HarshranjanN@users.noreply.github.com>
2021-10-18 19:47:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+
+var trex ,trex_running;
+function preload(){
+  trex_running=loadAnimation("trex1.png","trex3.png","trex4.png")
+
+}
+
+function setup(){
+  createCanvas(600,200)
+  
+  //create a trex sprite
+ trex=createSprite(50,160,20,50)
+ trex.scale=0.6
+ trex.addAnimation("running",trex_running)
+}
+
+function draw(){
+  background("white")
+  if(keyDown("space"))
+  trex.velocityY=-10
+  trex.velocityY=trex.velocityY+0.8
+drawSprites()
+}
